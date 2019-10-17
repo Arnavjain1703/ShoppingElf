cb03f1a2407668d7d7e23a4327de4fbd9f8a691e
rishabhtyagi2306 <rishabhtyagi.2306@gmail.com>
2019-10-17 10:23:32 +0530

    performed web api CRUD

---
+++ ShoppingELF/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using ShoppingELF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoppingELF.Controllers
+{
+    public class AccountController : ApiController
+    {
+        public HttpResponseMessage PostSignup([FromBody]User user)
+        {
+            try
+            {
+                using(DataContext db = new DataContext())
+                {
+                    db.users.Add(user);
+                    db.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, user);
+                    message.Headers.Location = new Uri(Request.RequestUri + user.UserID.ToString());
+                    return message;
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage GetVerifyAccount(string id, User model)
+        {
+            bool status = false;
+            using (var context = new DataContext())
+            {
+                User us = new User();
+                var v = context.users.Where(a => a.ActivationCode == new Guid(id)).FirstOrDefault();
+                if(v != null)
+                {
+                    us.IsEmailVerified = true;
+                    v.IsEmailVerified = Convert.ToBoolean(us.IsEmailVerified);
+                    context.SaveChanges();
+                    status = true;
+                }
+                //Session["IsEmailVerified"] = model.IsEmailVerified;
+
+            }
+            return status;
+
+        }
+    }
+}
---
+++ ShoppingELF/Controllers/UsersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ShoppingELF.Models;
+
+namespace ShoppingELF.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Users
+        public IQueryable<User> Getusers()
+        {
+            return db.users;
+        }
+
+        // GET: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(int id)
+        {
+            User user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUser([FromBody]int id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.UserID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(User))]
+        public IHttpActionResult PostUser([FromBody]User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.users.Add(user);
+            db.SaveChanges();
+
+            return CreatedAtRoute("PostUser", new { id = user.UserID }, user);
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUser(int id)
+        {
+            User user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(int id)
+        {
+            return db.users.Count(e => e.UserID == id) > 0;
+        }
+    }
+}
